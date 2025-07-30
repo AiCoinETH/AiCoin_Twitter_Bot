d604ea2d183721444c148ce39808785ec312571d
AiCoinETH <mr.greencrystal@gmail.com>
2025-07-30 17:20:29 +0300

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,15 +3,15 @@ from telegram import Bot
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-# Замените на ваш токен и ID/username канала
-TOKEN = "ВАШ_ТОКЕН_БОТА"
+TOKEN = "ВАШ_ТОКЕН_БОТА"  # Ваш токен
 CHANNEL_ID = "-1002868465126"  # Или "@AiCoin_ETH"
 
 def main():
     bot = Bot(token=TOKEN)
+    logging.info(f"Пробуем отправить сообщение в канал: {CHANNEL_ID}")
     try:
-        message = bot.send_message(chat_id=CHANNEL_ID, text="Тест публикации из telegram_channel_test.py")
-        logging.info(f"Сообщение успешно отправлено, ID: {message.message_id}")
+        message = bot.send_message(chat_id=CHANNEL_ID, text="Тест публикации из скрипта")
+        logging.info(f"Сообщение отправлено успешно, ID: {message.message_id}")
     except Exception as e:
         logging.error(f"Ошибка при отправке сообщения: {e}")
 
