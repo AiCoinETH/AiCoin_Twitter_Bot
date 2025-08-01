d8fa6ee29263ec7e7d2d3665611a541daff0c202
AiCoinETH <mr.greencrystal@gmail.com>
2025-08-01 14:27:35 +0300

    Update twitter_stat_bot.py

--- twitter_stat_bot.py
+++ twitter_stat_bot.py
@@ -1,22 +1,19 @@
 import os
-import asyncio
 from telegram import Bot
 
-TELEGRAM_TOKEN = 'ТВОЙ_ТОКЕН'
-CHANNEL_ID = '@AiCoin_ETH'  # или '-100xxx...' для приватного
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN_CHANNEL')
+CHANNEL_ID = os.environ.get('TELEGRAM_CHANNEL_USERNAME_ID')
 
-async def main():
-    bot = Bot(token=TELEGRAM_TOKEN)
-    text = (
-        "🕊️ [Twitter](https://x.com/AiCoin_ETH): 0 подписчиков\n"
-        "🌐 [Сайт](https://getaicoin.com/)"
-    )
-    msg = await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=text,
-        parse_mode='Markdown'
-    )
-    print("Message ID:", msg.message_id)
+bot = Bot(token=TELEGRAM_TOKEN)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+text = (
+    "🕊️ [Twitter](https://x.com/AiCoin_ETH): 0 подписчиков\n"
+    "🌐 [Сайт](https://getaicoin.com/)"
+)
+msg = bot.send_message(
+    chat_id=CHANNEL_ID,
+    text=text,
+    parse_mode='Markdown'
+)
+
+print("Message ID:", msg.message_id)
